62ae963e09745e559596ac45af3a1ec6830b7416
Woollywood <115595112+Woollywood@users.noreply.github.com>
2024-08-22 07:38:30 +0300

    feat: sidebar channels

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
 
-import { Navbar } from '@/components/navbar';
+import Sidebar from '@/widgets/sidebar';
 
 export default function DefaultLayout() {
 	return (
-		<div className='relative flex h-screen flex-col'>
-			<Navbar />
+		<div className='relative grid h-screen grid-cols-[18rem_1fr]'>
+			<Sidebar />
 			<main className='container mx-auto max-w-7xl flex-grow px-6 pt-16'>
 				<Outlet />
 			</main>
