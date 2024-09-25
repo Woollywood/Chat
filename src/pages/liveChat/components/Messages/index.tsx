a6e8bc332b53648aeff34f5885d64cc783bcabc0
Woollywood <115595112+Woollywood@users.noreply.github.com>
2024-09-25 21:32:44 +0300

    refactor: message controls - textarea

--- src/pages/liveChat/components/Messages/index.tsx
+++ src/pages/liveChat/components/Messages/index.tsx
@@ -61,7 +61,6 @@ export default function Messages() {
 
 	async function handleSend() {
 		const trimmedMessage = message.trim();
-		console.log(trimmedMessage);
 
 		if (trimmedMessage.length > 0) {
 			setLoadingMessages(true);
